2fbfb51be98f05a4e6fc4c9edb6d3a2a0b65473f
Mirko Denecke <mirkix@gmail.com>
2021-06-06 12:09:56 +0200

    add ethtrigger module

---
+++ MAVProxy/modules/mavproxy_ethtrigger.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import os
+import os.path
+import sys
+from pymavlink import mavutil
+import errno
+import socket
+
+from MAVProxy.modules.lib import mp_module
+from MAVProxy.modules.lib import mp_util
+from MAVProxy.modules.lib import mp_settings
+
+
+class ethtrigger(mp_module.MPModule):
+    def __init__(self, mpstate):
+        """Initialise module"""
+        super(ethtrigger, self).__init__(mpstate, "ethtrigger", "")
+
+        TCP_IP = '192.168.10.177'
+        TCP_PORT = 23
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((TCP_IP, TCP_PORT))
+
+        self.ethtrigger_settings = mp_settings.MPSettings(
+            [ ('verbose', bool, False),
+              ('steps', int, -1000),
+          ])
+        self.add_command('ethtrigger', self.cmd_ethtrigger, "ethtrigger module", ['status','set'])
+
+    def usage(self):
+        '''show help on command line options'''
+        return "Usage: ethtrigger <status|set verbose|set steps>"
+
+    def cmd_ethtrigger(self, args):
+        '''control behaviour of the module'''
+        if len(args) == 0:
+            print(self.usage())
+        elif args[0] == "status":
+            print(self.status())
+        elif args[0] == "set":
+            self.ethtrigger_settings.command(args[1:])
+        else:
+            print(self.usage())
+
+    def status(self):
+        '''returns information about module'''
+        return("steps: " + str(self.ethtrigger_settings.steps))
+
+    def mavlink_packet(self, m):
+        '''handle mavlink packets'''
+        if m.get_type() == 'CAMERA_FEEDBACK':
+            if self.ethtrigger_settings.verbose:
+                print("CAMERA_FEEDBACK")
+
+            MESSAGE = str(self.ethtrigger_settings.steps) + "\r"
+            self.s.send(MESSAGE.encode())
+
+def init(mpstate):
+    '''initialise module'''
+    return ethtrigger(mpstate)
