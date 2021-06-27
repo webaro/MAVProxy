c9f2e120af9f46bbea11e79dc8faf71caaf6a300
Mirko Denecke <mirkix@gmail.com>
2021-06-27 17:14:49 +0200

    ethtrigger reconnect

--- MAVProxy/modules/mavproxy_ethtrigger.py
+++ MAVProxy/modules/mavproxy_ethtrigger.py
@@ -11,17 +11,22 @@ from MAVProxy.modules.lib import mp_module
 from MAVProxy.modules.lib import mp_util
 from MAVProxy.modules.lib import mp_settings
 
+TCP_IP = '192.168.15.50'
+TCP_PORT = 23
 
 class ethtrigger(mp_module.MPModule):
     def __init__(self, mpstate):
         """Initialise module"""
         super(ethtrigger, self).__init__(mpstate, "ethtrigger", "")
 
-        TCP_IP = '192.168.15.50'
-        TCP_PORT = 23
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect((TCP_IP, TCP_PORT))
-
+        try:
+            self.s.settimeout(1)
+            self.s.connect((TCP_IP, TCP_PORT))
+            self.s.settimeout(None)
+        except:
+            pass
+        
         self.ethtrigger_settings = mp_settings.MPSettings(
             [ ('verbose', bool, False),
               ('steps', int, 800),
@@ -54,7 +59,16 @@ class ethtrigger(mp_module.MPModule):
                 print("CAMERA_FEEDBACK")
 
             MESSAGE = str(self.ethtrigger_settings.steps) + "\r"
-            self.s.send(MESSAGE.encode())
+            try:
+                self.s.send(MESSAGE.encode())
+            except:
+                # recreate the socket and reconnect
+                self.s.close()
+                self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.s.settimeout(1)
+                self.s.connect((TCP_IP, TCP_PORT))
+                self.s.settimeout(None)
+                self.s.send(MESSAGE.encode())
 
 def init(mpstate):
     '''initialise module'''
