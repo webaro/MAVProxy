71790fb4533f1d00235e65e578b79b4c1ed4a397
Mirko Denecke <mirkix@gmail.com>
2022-03-30 07:45:43 +0100

    add dual lift support

--- MAVProxy/modules/mavproxy_lift.py
+++ MAVProxy/modules/mavproxy_lift.py
@@ -12,7 +12,8 @@ from MAVProxy.modules.lib import mp_module
 from MAVProxy.modules.lib import mp_util
 from MAVProxy.modules.lib import mp_settings
 
-TCP_IP = '192.168.15.60'
+TCP_IP_1 = '192.168.15.60'
+TCP_IP_2 = '192.168.15.61'
 TCP_PORT = 23
 
 class lift(mp_module.MPModule):
@@ -23,19 +24,28 @@ class lift(mp_module.MPModule):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.s.settimeout(1)
-            self.s.connect((TCP_IP, TCP_PORT))
+            self.s.connect((TCP_IP_1, TCP_PORT))
             self.s.settimeout(None)
+        except:
+            pass
+
+        self.s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.s1.settimeout(1)
+            self.s1.connect((TCP_IP_2, TCP_PORT))
+            self.s1.settimeout(None)
         except:
             pass
 
         self.lift_settings = mp_settings.MPSettings(
             [ ('verbose', bool, False),
-              ('time', int, 2000),
+              ('up', int, -1),
+              ('down', int, -35000),
               ('speed', int, 200),
               ('mode', int, 2),                  # mode: 0=do nothing, 1=seed, 2=seed and collect data, 3=chop weeds
           ])
         self.lift_settings.set_callback(self.set_callback)
-        self.add_command('lift', self.cmd_lift, "lift module", ['status','set','up', 'down'])
+        self.add_command('lift', self.cmd_lift, "lift module", ['status','set','up', 'down', 'home'])
         self.simstate = 0
 
     def usage(self):
@@ -54,6 +64,10 @@ class lift(mp_module.MPModule):
             self.lift_up()
         elif args[0] == "down":
             self.lift_down()
+        elif args[0] == "home":
+            self.lift_home()
+        elif args[0] == "stop":
+            self.lift_stop()
         else:
             print(self.usage())
 
@@ -76,29 +90,72 @@ class lift(mp_module.MPModule):
                 self.s.close()
                 self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.s.settimeout(1)
-                self.s.connect((TCP_IP, TCP_PORT))
+                self.s.connect((TCP_IP_1, TCP_PORT))
                 self.s.settimeout(None)
                 self.s.send(MESSAGE.encode())
 
+            try:
+                self.s1.send(MESSAGE.encode())
+            except:
+                # recreate the socket and reconnect
+                self.s1.close()
+                self.s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.s1.settimeout(1)
+                self.s1.connect((TCP_IP_2, TCP_PORT))
+                self.s1.settimeout(None)
+                self.s1.send(MESSAGE.encode())
+
     def lift_down(self):
-        message = "1t-" + str(self.lift_settings.time) + "\r"
+        message = "1t" + str(self.lift_settings.down) + "\r"
+        self.send_lift(message)
+        message = "2t" + str(self.lift_settings.down) + "\r"
+        self.send_lift(message)
+        message = "3t" + str(self.lift_settings.down) + "\r"
         self.send_lift(message)
 
     def lift_up(self):
-        message = "1t" + str(self.lift_settings.time) + "\r"
+        message = "1t" + str(self.lift_settings.up) + "\r"
+        self.send_lift(message)
+        message = "2t" + str(self.lift_settings.up) + "\r"
+        self.send_lift(message)
+        message = "3t" + str(self.lift_settings.up) + "\r"
         self.send_lift(message)
 
     def lift_home(self):
         message = "1h\r"
         self.send_lift(message)
+        message = "2h\r"
+        self.send_lift(message)
+        message = "3h\r"
+        self.send_lift(message)
+
+    def lift_stop(self):
+        message = "1b\r"
+        self.send_lift(message)
+        message = "2b\r"
+        self.send_lift(message)
+        message = "3b\r"
+        self.send_lift(message)
 
     def lift_speed(self, speed):
         message = "1s" + str(speed) + "\r"
+        self.send_lift(message)
+        message = "2s" + str(speed) + "\r"
+        self.send_lift(message)
+        message = "3s" + str(speed) + "\r"
         self.send_lift(message)
 
     def mavlink_packet(self, m):
         '''handle mavlink packets'''
         if m.get_type() == "STATUSTEXT":
+            if m.text == "lift stop":
+                print("lift stop")
+                self.lift_stop()
+
+            if m.text == "lift home":
+                print("lift home")
+                self.lift_home()
+
             if m.text == "lift up":
                 print("lift up")
                 self.lift_up()
@@ -110,13 +167,6 @@ class lift(mp_module.MPModule):
         # do nothing if mode = 0
         if self.lift_settings.mode == 0:
             return
-
-        if m.get_type() == "COMMAND_LONG":
-            print ("Get Long")
-            if m.command == mavutil.mavlink.MAV_CMD_DO_DIGICAM_CONFIGURE:
-                print ("Got Message Digicam_configure")
-            elif m.command == mavutil.mavlink.MAV_CMD_DO_DIGICAM_CONTROL:
-                print ("Got Message Digicam_control")
 
         if m.get_type() == 'SIMSTATE':
             self.simstate = 1
